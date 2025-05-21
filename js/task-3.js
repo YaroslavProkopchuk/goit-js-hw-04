67126c98f00262b5caa9e9888a3d89dbfd819e21
Yaroslav Prokopchuk <8840007@gmail.com>
2025-05-21 10:44:37 +0300

    blank

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  const valNum = [];
+  for (const zifer of numbers) {
+    if (zifer > value) {
+      valNum.push(zifer);
+    }
+  }
+  return valNum;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
